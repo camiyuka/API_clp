f4a7e807cc66d371c2dd36895b9fcf1c0692a704
camilayatabe <camila.yatabe@gmail.com>
2024-03-14 20:10:13 -0300

    chore(update): update models

--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class ApiModel(models.Model):
-    sensor = models.BooleanField(default=False)
-    botao = models.BooleanField(default=False)
-    liga_robo = models.BooleanField(default=False)
-    reset_contador = models.BooleanField(default=False)
-    valor_contagem = models.IntegerField(default=0)
+    Sensor = models.BooleanField(default=False)
+    Botao = models.BooleanField(default=False)
+    LigaRobo = models.BooleanField(default=False)
+    ResetContador = models.BooleanField(default=False)
+    Valor_Contagem = models.IntegerField(default=0)
 
     def __str__(self):
             return "ApiModel object"
+
 
