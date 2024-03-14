92369df3c6d261b402ce64e20222a8802a1e8bd3
camilayatabe <camila.yatabe@gmail.com>
2024-03-14 19:38:48 -0300

    fix(views): remove many=True to get only one object

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ from django.http import JsonResponse
 class ApiModelViewSet(viewsets.ViewSet):
     def list(self, request):
         api_models = ApiModel.objects.last()
-        serializer= ApiModelSerializer(api_models, many= True)
+        serializer= ApiModelSerializer(api_models)
         return render(request, 'api_model_view.html', {'api_models': serializer.data})
 
 class DeviceS7View(viewsets.ModelViewSet):
